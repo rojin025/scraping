660b23fd56554c47df9358fdbb33fdc6eec7ac51
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-13 17:53:14 -0600

    working on bookData Function

--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log("Hello Word!");
+const puppeteer = require("puppeteer");
+import { Browser } from "puppeteer";
+
+const url = "https://books.toscrape.com";
+
+async function main(url: string) {
+  const browser: Browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // evaluate - exam the whole page
+  const bookData = await page.evaluate(() => {
+    const bookPods = Array.from(document.querySelectorAll(".product_pod"));
+    // const data = bookPods.map((book: any) => {
+    //     title:
+    // })
+    return bookPods;
+  });
+  console.log(bookData);
+
+  await browser.close();
+}
+
+main(url);
